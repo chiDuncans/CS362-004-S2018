fb46a2b9ab46824663c84bea598b44f08d9e334a
Chieko Duncans <duncansc@flip3.engr.oregonstate.edu>
2018-05-16 18:24:45 -0700

    modified randomTestAdventurer.c

--- projects/duncansc-2/dominion/randomTestAdventurer.c
+++ projects/duncansc-2/dominion/randomTestAdventurer.c
@@ -29,6 +29,8 @@
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"
+#include <time.h>
+#include <math.h>
 
 #define DEBUG 0
 #define NOISY_TEST 1
@@ -37,176 +39,144 @@
 // #define MAX_HAND 500  //declared in dominion.h
 // #define MAX_DECK 500  //declared in dominion.h
 
-
-void supplyRandomCardNums(int p, struct gameState *state){
-
-    // set random hand card
-    state->handCount[p] = floor(Random() * MAX_HAND);
-    	
-    // set random deck card
-    state->deckCount[p] = floor(Random() * MAX_DECK);
-
-    // set random discard card
-    state->discardCount[p] = floor(Random() * MAX_DECK);
-}
-
 void checkDrawCard(int p, struct gameState *post) {
-    int r;
+	int r;
 
-    r = drawCard (p, post);
+	r = drawCard (p, post);
 
-    assert (r == 0);
+	assert (r == 0);
 }
 
-void checkHandCount(int preHand, int _p, struct gameState *_post) {
-    /*
-       if(_post->handCount[_p] != (preHand + 2)){
-       return -1;
-       }
-       else{
-       return 0;
-       }
-       */
-    assert(_post->handCount[_p] == (preHand + 2));
+int checkHandCount(int preHand, int _p, struct gameState *_post) {
+
+	if(_post->handCount[_p] != (preHand + 2)){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+	// assert(_post->handCount[_p] == (preHand + 2));
 }
 
-void checkDeckCount(int preDeck, int _p, struct gameState *_post) {
-    /*
-       if(_post->deckCount[_p] != (preDeck -2)){
-       return -1;
-       }
-       else{
-       return 0;
-       }
-       */
-    assert(_post->deckCount[_p] == (preDeck -2));
+int checkDeckCount(int preDeck, int _p, struct gameState *_post) {
+
+	if(_post->deckCount[_p] != (preDeck -2)){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+
+	//   assert(_post->deckCount[_p] == (preDeck -2));
 }
 
-void checkDiscardCount(int _p, struct gameState *_post) {
-    /*
-       if(_post->discardCount[_p] != 0){
-       return -1;
-       }
-       else{
-       return 0;
-       }
-       */
-    assert(_post->discardCount[_p] == 0);
+int checkDiscardCount(int _p, struct gameState *_post) {
+
+	if(_post->discardCount[_p] != 0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+
+	//   assert(_post->discardCount[_p] == 0);
 }
 
-void checkTotalCount(int preHand, int preDeck, int _p, struct gameState *_post) {
-    /*
-       if((_post->handCount[_p] + _post->deckCount[_p]) != (preHand + preDeck)){
-       return -1;
-       }
-       else{
-       return 0;
-       }
-       */
+int checkTotalCount(int preHand, int preDeck, int _p, struct gameState *_post) {
 
-    assert((_post->handCount[_p] + _post->deckCount[_p]) == (preHand + preDeck));
+	if((_post->handCount[_p] + _post->deckCount[_p]) != (preHand + preDeck)){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+
+
+	//    assert((_post->handCount[_p] + _post->deckCount[_p]) == (preHand + preDeck));
 }
 
 void checkCardEffectAdventurer(int p, struct gameState *post) {
-    int r;
-      printf ("pre hand count, player 0: %d \n", post->handCount[p]);
-    //  printf ("pre deck count, player 0: %d \n", post->deckCount[p]);
-    //  printf ("pre discard count, player 0: %d \n", post->discardCount[p]);
-    r = cardEffectAdventurer(0, post, p, 0); // (drawntreasure = 0, state, currentPlayer, int z = 0)
+	int r;
+	printf ("pre hand count, player 0: %d \n", post->handCount[p]);
+	printf ("pre deck count, player 0: %d \n", post->deckCount[p]);
+	printf ("pre discard count, player 0: %d \n", post->discardCount[p]);
+	r = cardEffectAdventurer(0, post, p, 0); // (drawntreasure = 0, state, currentPlayer, int z = 0)
 
-    assert (r == 0);  // Checking calling the function has error or not
+	assert (r == 0);  // Checking calling the function has error or not
 
-    //  if( r == 0){  // Checking calling the function has error or not
-      printf ("post hand count, player 0: %d \n", post->handCount[p]);
-    //  printf ("post deck count, player 0: %d \n", post->deckCount[p]);
-    //  printf ("post discard count, player 0: %d \n", post->discardCount[p]);
-    //  }
+	//  if( r == 0){  // Checking calling the function has error or not
+	printf ("post hand count, player 0: %d \n", post->handCount[p]);
+	printf ("post deck count, player 0: %d \n", post->deckCount[p]);
+	printf ("post discard count, player 0: %d \n", post->discardCount[p]);
+	//  }
 
 }
 
 int main () {
 
-    int i, n, r, p, deckCount, discardCount, handCount, error;
+	int i, n, r, p, deckCount, discardCount, handCount, error;
 
-    int k[10] = {adventurer, council_room, feast, gardens, mine,
-	remodel, smithy, village, baron, great_hall};
+	int k[10] = {adventurer, council_room, feast, gardens, mine,
+		remodel, smithy, village, baron, great_hall};
 
-    struct gameState G;
-    error = 0;
+	struct gameState G;
 
-    printf ("Testing Adventurer Card.\n");
+	//   srand(time(NULL));
+	error = 0;
 
-    printf ("RANDOM TESTS.\n");
+	printf ("Testing Adventurer Card.\n");
 
-    // initialize game for valid number of players, 2 - 4
-    for(n = 2; n <= MAX_PLAYERS; n++){
-	
-        // assign random testing values
+	printf ("RANDOM TESTS.\n");
 
-	p = 0; // player
-	initializeGame(n, k, 1, &G);
-	
- 	supplyRandomCardNums(p, &G);
+	SelectStream(2);
+	PutSeed(3);
 
-	handCount = G.handCount[p];
-	deckCount = G.deckCount[p];
-	discardCount = G.discardCount[p];
-	//  printf ("pre hand count, player 0: %d \n", G.handCount[p]);
-	//  printf ("pre deck count, player 0: %d \n", G.deckCount[p]);
-	//  printf ("pre discard count, player 0: %d \n", G.discardCount[p]);
-	//  cardEffectAdventurer(0, &G, p, 0); // (drawntreasure = 0, state, currentPlayer, int z = 0)
-	checkCardEffectAdventurer(p, &G);
+	// initialize game for valid number of players, 2 - 4
+	for(n = 2; n <= MAX_PLAYERS; n++){
 
-	checkHandCount(handCount, p, &G);
-	checkDeckCount(deckCount, p, &G);
-	checkDiscardCount(p, &G);
-	checkTotalCount(handCount, deckCount, p, &G);
-    } 
 
-    /*
-       SelectStream(2);
-       PutSeed(3);
 
-       for (n = 0; n < 2000; n++) {
-       for (i = 0; i < sizeof(struct gameState); i++) {
-       ((char*)&G)[i] = floor(Random() * 256);
-       }
-       p = floor(Random() * 2);
-       G.deckCount[p] = floor(Random() * MAX_DECK);
-       G.discardCount[p] = floor(Random() * MAX_DECK);
-       G.handCount[p] = floor(Random() * MAX_HAND);
-       checkDrawCard(p, &G);
-       }
+		p = n-2; // player
+		initializeGame(n, k, 1, &G);
 
-       printf ("ALL TESTS OK\n");
 
-       exit(0);
+		// for each players, do random test for MAX test times
+		//added
 
-       printf ("SIMPLE FIXED TESTS.\n");
-       for (p = 0; p < 2; p++) {
-       for (deckCount = 0; deckCount < 5; deckCount++) {
-       for (discardCount = 0; discardCount < 5; discardCount++) {
-       for (handCount = 0; handCount < 5; handCount++) {
-       memset(&G, 23, sizeof(struct gameState)); 
-       r = initializeGame(2, k, 1, &G);
-       G.deckCount[p] = deckCount;
-       memset(G.deck[p], 0, sizeof(int) * deckCount);
-       G.discardCount[p] = discardCount;
-       memset(G.discard[p], 0, sizeof(int) * discardCount);
-       G.handCount[p] = handCount;
-       memset(G.hand[p], 0, sizeof(int) * handCount);
-       checkDrawCard(p, &G);
-       }
-       }
-       }
-       }
-       */
-    if(error == 0){
-	printf ("ALL TESTS PASSED\n");
-    }
-    else{
+		for(i = 0; i < MAX_TESTS; i++){
+			G.deckCount[p] = floor(Random() * 10) + 5;
+			G.discardCount[p] = floor(Random() * 10) + 5;
+			G.handCount[p] = floor(Random() * 5);
+			//added end
+			handCount = G.handCount[p];
+			deckCount = G.deckCount[p];
+			discardCount = G.discardCount[p];
+			//  printf ("pre hand count, player 0: %d \n", G.handCount[p]);
+			//  printf ("pre deck count, player 0: %d \n", G.deckCount[p]);
+			//  printf ("pre discard count, player 0: %d \n", G.discardCount[p]);
+			//  cardEffectAdventurer(0, &G, p, 0); // (drawntreasure = 0, state, currentPlayer, int z = 0)
+			checkCardEffectAdventurer(p, &G);
 
-	printf ("Functional Errors: %d\n", error);
-    }
-    return 0;
+			// to catch number of error, following checks are modified
+			/*
+			   checkHandCount(handCount, p, &G);
+			   checkDeckCount(deckCount, p, &G);
+			   checkDiscardCount(p, &G);
+			   checkTotalCount(handCount, deckCount, p, &G);
+			   */  
+			if(checkHandCount(handCount, p, &G) != 0){ error++; }
+			//error = error + checkDeckCount(deckCount, p, &G);
+			//error = error + checkDiscardCount(p, &G);
+			//error = error + checkTotalCount(handCount, deckCount, p, &G);
+		}
+	} 
+
+	if(error == 0){
+		printf ("ALL TESTS PASSED\n");
+	}
+	else{
+
+		printf ("Functional Errors: %d\n", error);
+	}
+	return 0;
 }
